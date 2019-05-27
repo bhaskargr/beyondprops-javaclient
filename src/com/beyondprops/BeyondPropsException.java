eba2826ade49c762a9af0b76583863dc811fbd07
Bhaskar Gandavabi <bhaskar.gandavabi@gmail.com>
2019-05-27 00:39:19 -0400

    Add files via upload

---
+++ src/com/beyondprops/BeyondPropsException.java
@@ -0,0 +1,14 @@
+package com.beyondprops;
+
+public class BeyondPropsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1670471740973210457L;
+
+	public BeyondPropsException(String string) {
+		super(string);
+	}
+
+}
